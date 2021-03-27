191bf7aa78aed14ecf954f973b828bd92137b429
Martyn <zogacc@gmail.com>
2021-03-27 21:51:38 +0200

    hide dirty stats stuff

--- src/features/Home/Home.tsx
+++ src/features/Home/Home.tsx
@@ -5,11 +5,11 @@ import {
   TimerButton,
   Countdown,
   Noise,
-  Stats,
+  // Stats,
 } from "features";
-import { getFibonacciDiscrete } from "shared/utils";
+// import { getFibonacciDiscrete } from "shared/utils";
 import styles from "./Home.module.css";
-import { StatsData } from "shared/types";
+// import { StatsData } from "shared/types";
 
 // TODO: refactor this component
 export const Home: React.FC = () => {
@@ -39,30 +39,30 @@ export const Home: React.FC = () => {
     setCurrentTimerId(timerId);
   }, []);
 
-  const writeSessionToStore = React.useCallback((seconds) => {
-    const dateObj = new Date();
-    const month = String(dateObj.getMonth() + 1).padStart(2, "0");
-    const day = String(dateObj.getDate()).padStart(2, "0");
-    const year = dateObj.getFullYear();
-    const date = `${year}-${month}-${day}`;
+  // const writeSessionToStore = React.useCallback((seconds) => {
+  //   const dateObj = new Date();
+  //   const month = String(dateObj.getMonth() + 1).padStart(2, "0");
+  //   const day = String(dateObj.getDate()).padStart(2, "0");
+  //   const year = dateObj.getFullYear();
+  //   const date = `${year}-${month}-${day}`;
 
-    const totalTime = getFibonacciDiscrete(seconds / 60);
+  //   const totalTime = getFibonacciDiscrete(seconds / 60);
 
-    const dataString = localStorage.getItem("stats") ?? "{}";
-    const statsData: StatsData = JSON.parse(dataString);
-    const item = statsData[date] || [];
-    const newItem = [...item, totalTime];
-    const newData = { ...statsData, [date]: newItem };
+  //   const dataString = localStorage.getItem("stats") ?? "{}";
+  //   const statsData: StatsData = JSON.parse(dataString);
+  //   const item = statsData[date] || [];
+  //   const newItem = [...item, totalTime];
+  //   const newData = { ...statsData, [date]: newItem };
 
-    const newDataString = JSON.stringify(newData);
-    localStorage.setItem("stats", newDataString);
-  }, []);
+  //   const newDataString = JSON.stringify(newData);
+  //   localStorage.setItem("stats", newDataString);
+  // }, []);
 
   const handleTimerClick = React.useCallback(() => {
     setTimerDiff(0);
 
     if (isStarted) {
-      writeSessionToStore(timerDiff);
+      // writeSessionToStore(timerDiff);
 
       setStartedFlag(false);
       setTimestamp(0);
@@ -77,14 +77,14 @@ export const Home: React.FC = () => {
 
     setTimestamp(Math.round(Date.now() / 1000));
     tickTimer();
-  }, [currentTimerId, isStarted, tickTimer, timerDiff, writeSessionToStore]);
+  }, [currentTimerId, isStarted, tickTimer]);
 
-  const sessionsData = JSON.parse(localStorage.getItem("stats") ?? "[]");
+  // const sessionsData = JSON.parse(localStorage.getItem("stats") ?? "[]");
 
   return (
     <main className={styles["app-wrapper"]}>
       <Header />
-      <Stats sessionsData={sessionsData} />
+      {/* <Stats sessionsData={sessionsData} /> */}
       <Noise isTimerStarted={isStarted} />
       <TimerButton
         handleTimerClick={handleTimerClick}
--- src/features/index.ts
+++ src/features/index.ts
@@ -5,4 +5,3 @@ export { Header } from "./Header/Header";
 export { TimerButton } from "./TimerButton/TimerButton";
 export { Countdown } from "./Countdown/Countdown";
 export { Noise } from "./Noise/Noise";
-export { Stats } from "./Stats/Stats";
