670f740bcdbf79690048bec37b0961ad49bdcccc
Martyn <zogacc@gmail.com>
2021-03-27 22:07:59 +0200

    fix loud gong sound on meditation stage

--- src/features/Progress/Sound.tsx
+++ src/features/Progress/Sound.tsx
@@ -8,14 +8,18 @@ interface Props {
 }
 
 export const Sound: React.FC<Props> = ({ progress }) => {
-  const [playGong] = useSound(gongSfx);
+  const [playGong] = useSound(gongSfx, {
+    volume: 0.001 /* comfort volume value */,
+  });
 
+  // TODO: remake in custom hook instead of component
   React.useEffect(() => {
     const minutes = progress / 60;
     const isFibNum = fibonacciNumsForTimer.includes(minutes);
+    const playbackRate = 4 - minutes / 7; // ? for 21 it will be 1 — original playback rate
 
     if (isFibNum) {
-      playGong();
+      playGong({ playbackRate });
     }
   }, [progress, playGong]);
 
