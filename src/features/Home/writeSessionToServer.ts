a0941e49628f175dd7ecaf1109eef210f3d8d9b6
Martyn <zogacc@gmail.com>
2021-07-11 14:30:11 +0200

    fix date format

--- src/features/Home/writeSessionToServer.ts
+++ src/features/Home/writeSessionToServer.ts
@@ -4,7 +4,14 @@ import { getFibonacciDiscrete } from "shared/utils";
 
 export const writeSessionToServer = (seconds: number) => {
   // NOTE: to G.Sheets std date+time format: <dd/mm/yyyy hh:mm:ss>
-  const date = new Date().toLocaleString().replace(",", "");
+  const dateObj = new Date();
+  const month = String(dateObj.getMonth() + 1).padStart(2, "0");
+  const day = String(dateObj.getDate()).padStart(2, "0");
+  const year = dateObj.getFullYear();
+  const hours = String(dateObj.getHours() + 1).padStart(2, "0");
+  const minutes = String(dateObj.getMinutes() + 1).padStart(2, "0");
+  const secs = String(dateObj.getSeconds() + 1).padStart(2, "0");
+  const date = `${day}/${month}/${year} ${hours}:${minutes}:${secs}`;
 
   const totalTime = getFibonacciDiscrete(seconds / 60);
   const sessionData = {
