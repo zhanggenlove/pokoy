483e9671561f958ec331cc0cee335324b36362f4
Martyn <zogacc@gmail.com>
2021-11-25 13:41:35 +0200

    PWA updater (#24)
    * extract function
    
    * add dep for service worker updating
    
    * remove redundant imports
    
    * remove redundant global styles
    
    * set code formatter to remove all semicolons

--- src/features/Home/Home.tsx
+++ src/features/Home/Home.tsx
@@ -1,21 +1,22 @@
-import React from "react";
-import styles from "./Home.module.css";
-import { auth } from "features/Home/firebase-init";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { Pokoy } from "features/Pokoy/Pokoy";
-import { useOnline } from "@saulpalv/useonline";
-import { OfflineStatus } from "./OfflineStatus";
-import { SignIn } from "features/SignIn/SignIn";
-import { FibLoader } from "features/fib-loader";
+import React from "react"
+import styles from "./Home.module.css"
+import { auth } from "features/Home/firebase-init"
+import { useAuthState } from "react-firebase-hooks/auth"
+import { Pokoy } from "features/Pokoy/Pokoy"
+import { useOnline } from "@saulpalv/useonline"
+import { OfflineStatus } from "./OfflineStatus"
+import { SignIn } from "features/SignIn/SignIn"
+import { FibLoader } from "features/fib-loader"
+import { AppUpdater } from "features/app-updater"
 
 // TODO: refactor this component
 export const Home: React.FC = () => {
-  const [user, loading] = useAuthState(auth);
-  const isOnline = useOnline();
+  const [user, loading] = useAuthState(auth)
+  const isOnline = useOnline()
 
-  const isStillLoading = loading && !user;
-  const userNotExist = !loading && !user;
-  const userExist = !loading && user;
+  const isStillLoading = loading && !user
+  const userNotExist = !loading && !user
+  const userExist = !loading && user
 
   return (
     <main className={styles["app-wrapper"]}>
@@ -26,6 +27,8 @@ export const Home: React.FC = () => {
       {userExist && <Pokoy user={user} />}
 
       {!isOnline && <OfflineStatus />}
+
+      <AppUpdater />
     </main>
-  );
-};
+  )
+}
---
+++ src/features/app-updater/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+
+export const Wrapper = styled.div`
+  width: 100%;
+  position: absolute;
+  top: 0;
+  padding: 1rem;
+`
+
+export const StyledButton = styled.button`
+  display: inline;
+`
