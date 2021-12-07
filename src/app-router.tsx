3966d144b2a7da732a9ef9a79495de3687bc2058
Martyn <zogacc@gmail.com>
2021-12-07 11:27:59 +0200

    remove stats route

--- src/app-router.tsx
+++ src/app-router.tsx
@@ -1,13 +1,11 @@
 import { App } from "features/app"
 import { DevPage } from "features/dev-page"
-import { UserStats } from "features/user-stats/user-stats"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 export const AppRouter = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
-      <Route path="stats" element={<UserStats />} />
       <Route path="dev" element={<DevPage />} />
       {/* // TODO: add route for authentication */}
       {/* <Route path="sign-in" element={<App />} /> */}
