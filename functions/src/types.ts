664846ee4a50a43edb5a6e297b984f74b16830c8
Martyn <zogacc@gmail.com>
2021-12-12 14:12:09 +0200

    add field for interface

--- functions/src/types.ts
+++ functions/src/types.ts
@@ -1,26 +1,26 @@
 // FIXME: duplicated types from pokoy-frontend sources
 
 export interface PokoySession {
-  duration: number
-  timestamp: string
+  duration: number;
+  timestamp: string;
   user?:
     | string
-    | FirebaseFirestore.DocumentReference<FirebaseFirestore.DocumentData>
-  userId: string
+    | FirebaseFirestore.DocumentReference<FirebaseFirestore.DocumentData>;
+  userId: string;
 }
 
 export interface UserStatsData {
-  totalDuration: number
-  count: number
-  userId: string
   firstMeditationDate: FirebaseFirestore.Timestamp | null
+  totalDuration: number;
+  count: number;
+  userId: string;
 }
 
 export interface DayData {
-  timestamp: FirebaseFirestore.Timestamp
-  totalDuration: number
-  count: number
-  meditations: PokoySession[]
-  userId: string
-  statsRef: FirebaseFirestore.DocumentReference<FirebaseFirestore.DocumentData>
+  timestamp: FirebaseFirestore.Timestamp;
+  totalDuration: number;
+  count: number;
+  meditations: PokoySession[];
+  userId: string;
+  statsRef: FirebaseFirestore.DocumentReference<FirebaseFirestore.DocumentData>;
 }
