f670137672c57ed2b02a045e25e8a2e5c9942b31
Martyn <zogacc@gmail.com>
2021-12-18 21:49:06 +0200

    extract and rework button for swipe views

---
+++ src/features/home/components/views-switcher/views-switcher.styles.ts
@@ -0,0 +1,28 @@
+import styled from "styled-components"
+
+export const SwipeButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  bottom: 0;
+  background-color: transparent;
+  color: var(--c-foreground);
+  transition: none;
+  margin: 1rem;
+  margin-bottom: 6rem;
+  border-radius: 3rem;
+`
+interface Props {
+  isActive: boolean
+}
+export const Circle = styled.span<Props>`
+  aspect-ratio: 1;
+  width: 1.5rem;
+  border-radius: 50%;
+  margin: 1rem;
+  display: block;
+  border: 0.25rem solid var(--c-extra-gray);
+  background-color: ${({ isActive }) =>
+    isActive ? "var(--c-foreground)" : "transparent"};
+`
